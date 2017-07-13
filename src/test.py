7480089c99a6c2d4fe6c2eb0bd31b53e42bb5ef4
Nicholas Brawand <nbrawand@Nicholass-MacBook-Pro-2.local>
2017-07-13 09:58:14 -0400

    moved notebook to main dir

--- src/test.py
+++ src/test.py
@@ -3,23 +3,19 @@
 #
 # Nicholas Brawand - nicholasbrawand@gmail.com
 #
-import tools
+from . import tools
 import math
 
 
-def CreateSamplePlot(X_train, y_train):
+def CreateSamplePlot(X_train, y_train, outFil):
     """Plot the first 10 samples in X_train with unique y_train"""
-    outFil = '../figures/mnist_all.png'
-    #print('testing image import see: ', outFil)
     imgs = []
     for i in range(10):
         imgs.append(X_train[y_train == i][0].reshape(28, 28))
     return tools.PlotGrid(2, 5, imgs, outFil)
 
-def PlotSingleKind(X_train, y_train, num, kind):
+def PlotSingleKind(X_train, y_train, num, kind, outFil):
     """plot first num samples in X_train with y_train==kind"""
-    outFil = '../figures/mnist_'+str(kind)+'.png'
-    #print('testing image import see: ', outFil)
     imgs = []
     tmp = X_train[y_train == kind]
     for i in range(num):
